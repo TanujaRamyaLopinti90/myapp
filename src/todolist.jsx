b952b6be0b84a426143111ffaee7815d1ba39384
tanujaramyalopinti90 <tanujaramya90@gmail.com>
2024-02-20 21:59:42 +0530

    tyhhui

---
+++ src/todolist.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+function Todolist() {
+    var [arr, setarr] = React.useState(["tanuja", "ramya"])
+    function abc(){
+        setarr([...arr,document.getElementById("d1").value])
+    }
+    return (
+        <div>
+            <h1>TODOLIST</h1>
+            <input type="text" id="d1"/>
+            <button onClick={abc}>ADD TODO</button>
+
+            {
+                arr.map((a)=> {
+                    return <li>{a}</li>
+                })
+            }
+        </div>
+    )
+}
+
+export default Todolist
