82558a2c62298464bb329c9596e7379ddfd25a81
tanujaramyalopinti90 <tanujaramya90@gmail.com>
2024-02-26 15:02:06 +0530

    gyuh

--- src/Arthmatic.jsx
+++ src/Arthmatic.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Third from "./Arthird";
 
 function Arth(){
     var ref1=React.useRef();
     var ref2=React.useRef();
+    var ref3=React.useRef();
     var [res,setRes]=React.useState(0)
     React.useEffect(()=>{
         ref1.current.focus();
@@ -16,8 +18,9 @@ function Arth(){
             <h1>Arth</h1>
             <input type="text" ref={ref1} onKeyUp={(e)=>{e.key==="Enter" && ref2.current.focus()}}/>
             <br />
-            <input type="text" ref={ref2} onKeyUp={(e)=>{e.key==="Enter" && add()}}/>
+            <input type="text" ref={ref2} onKeyUp={(e)=>{e.key==="Enter" && ref3.current.focus()}}/>
             <br/>
+        <Third ref={ref3}></Third>
             <button>Add</button>
             <h1>Result:{res}</h1>
         </div>
