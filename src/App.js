a550fda672f905bbbeb7a54a2322ef9e409dba07
tanujaramyalopinti90 <tanujaramya90@gmail.com>
2024-03-13 16:38:38 +0530

    fgh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="border border-2 border">
-      
+    <div className="border border-2 border-2 p-2 m-2">
+     <h1>Scratch-Redux</h1> 
     </div>
   );
 }
---
+++ src/store/counterReducer.js
@@ -0,0 +1,6 @@
+const initialState={
+    count:100
+}
+var counterReducer=function(state=initialState,action){
+    return state;  
+}
