d40a9226b13fb0fe165a4f12d9f91162ca4a7027
tanujaramyalopinti90 <tanujaramya90@gmail.com>
2024-03-14 17:34:29 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
   return (
     <div  className="border border-dark m-2 m-2 p-2">
       <h1>Redux-Toolkit</h1>
+      <h1>hai tanuja</h1>
       <Counter></Counter>
       <Todolist></Todolist>
     </div>
   );
-}
+}   
 
 export default App;
