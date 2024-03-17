c98af9e23d67758519bc70fec2e0399f40d54ca4
tanujaramyalopinti90 <tanujaramya90@gmail.com>
2024-03-17 13:17:26 +0530

    aaghjkl

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Counter from './features/counter/counter';
 function App() {
   return (
     <div  className="border border-dark m-2 m-2 p-2">
-      <h1>Redux-Toolkit</h1>
-      <h1>hai tanuja</h1>
+      <h1>Redux-Toolkit</h1>y
       <Counter></Counter>
       <Todolist></Todolist>
     </div>
